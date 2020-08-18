4218765f200f49064d9b2e60b82043b63cef1719
IvanKuragin <IvanKuraginWork@gmail.com>
2020-08-18 15:51:55 +0300

    Start pade 'photo'

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
 gulp.task('sass', function(){ // Создаем таск "sass"
     return gulp.src('project/sass/**/*.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('project/css')) // Выгружаем результата в папку app/css
+        .pipe(gulp.dest('project/css/')) // Выгружаем результата в папку app/css
 });
 
 gulp.task('watch', function() {
--- project/js/mobile-menu.js
+++ project/js/mobile-menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.main-nav__toggle').click(function(event) {
-		$('.main-nav__toggle,.main-nav__wrapper').toggleClass('active');
-		$('.index-header').toggleClass('index-header__padding')
+		$('.main-nav__toggle,.main-nav__wrapper,.burger-menu__span').toggleClass('active');
+		$('.index-header,.photo-header').toggleClass('active')
 	});
 });
